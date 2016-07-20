42bcc9ae698afd3e1da55e419e7d7fd337927747
Eric Gonzalez <Twinpaireric@gmail.com>
2016-07-19 21:54:59 -0700

    trivial

--- profile.php
+++ profile.php
@@ -5,12 +5,6 @@ require_once 'core/init.php';
 mysql_connect("127.13.13.2", "admin67KSwkr", "c6hsZzfY7pGR") or die(mysql_error());
 mysql_select_db("imag") or die(mysql_error());
 
-$elements = explode('/', $_SERVER['REQUEST_URI']);
-$account = $elements[1];
-echo $account . " " . $username;
-if (!$username)
-    if (!$username = Input::get('user'))
-        Redirect::to("/$account/");
 
 $user = new User($username);
 if (!$user->exists()) {
@@ -26,12 +20,8 @@ else {
 <head>
     <title>Imaguana | Profile</title>
     
-    <?php
-    $elements = explode('/', $_SERVER['REQUEST_URI']);
-    $account = $elements[1];
-    echo $account;
-    echo "<BASE href=\"/$account/\">";
-    ?>
+    <!-- CSS, Meta, Ajax, etc. -->
+    <?php include 'view/head.php' ?>
     
 </head>
 <body>
