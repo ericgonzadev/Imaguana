4f9a7b488de36a2724ff1b97e0525ec29d720b82
Eric Gonzalez <Twinpaireric@gmail.com>
2016-07-19 22:03:08 -0700

    trivial

--- profile.php
+++ profile.php
@@ -1,22 +1,9 @@
-<?php
-require_once 'core/init.php';
-
-//Connection
-mysql_connect("127.13.13.2", "admin67KSwkr", "c6hsZzfY7pGR") or die(mysql_error());
-mysql_select_db("imag") or die(mysql_error());
-
-?>
 
 <!doctype html>
 <head>
     <title>Imaguana | Profile</title>
     
-    <?php
-    $elements = explode('/', $_SERVER['REQUEST_URI']);
-    $account = $elements[1];
-    echo $account;
-    echo "<BASE href=\"/$account/\">";
-    ?>
+   
     
         
 </head>
