90a2668015fddbf51c4205ee2b7b39cd50af39f0
Eric Gonzalez <Twinpaireric@gmail.com>
2016-07-19 22:01:27 -0700

    trivial

--- profile.php
+++ profile.php
@@ -5,21 +5,6 @@ require_once 'core/init.php';
 mysql_connect("127.13.13.2", "admin67KSwkr", "c6hsZzfY7pGR") or die(mysql_error());
 mysql_select_db("imag") or die(mysql_error());
 
-$elements = explode('/', $_SERVER['REQUEST_URI']);
-$account = $elements[1];
-echo $account . " " . $username;
-if (!$username)
-    if (!$username = Input::get('user'))
-        Redirect::to("/$account/");
-
-$user = new User($username);
-if (!$user->exists()) {
-    Redirect::to(404);
-} 
-else {
-    $data = $user->data();
-    $user = new User();
-}
 ?>
 
 <!doctype html>
@@ -36,9 +21,6 @@ else {
         
 </head>
 <body>
-
-    <!-- Navigation Bar -->
-    <?php include 'view/nav-bar.php'; ?>
 
     <p>worked</p>
     
