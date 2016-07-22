e73574d6ae8ee17850cccbacc8244090b3362a9e
Eric Gonzalez <Twinpaireric@gmail.com>
2016-07-21 21:01:54 -0700

    trivial

--- controller.php
+++ controller.php
@@ -6,9 +6,9 @@ include_once 'controller/VideoController.php';
 $path = ltrim($_SERVER['REQUEST_URI'], '/');    // Trim leading slash(es)
 echo "path: ". $path. "\r\n";;
 $elements = explode('/', $path);                // Split path on slashes
-echo "elements: ". $elements . "\r\n";
+echo "elements: "; print_r($elements);  echo "\r\n";
 array_shift($elements);
-echo print_r(array_shift($elements)). "\r\n";;
+echo "array_shift($elements): " print_r(array_shift($elements)); echo "\r\n";
 switch(array_shift($elements)) {
     case 'image':
         $controller = new ImageController();
@@ -22,7 +22,7 @@ switch(array_shift($elements)) {
     default:
         break;
 }
-
+echo "controller: " print_r($controller); echo "\r\n";
 
 $action = array_shift($elements);
 $params = $elements;
