dc950c0e9dfdd7e66914652dd244facffb58d8b5
Eric Gonzalez <Twinpaireric@gmail.com>
2016-07-24 16:24:56 -0700

    debugging

--- imageresults.php
+++ imageresults.php
@@ -143,7 +143,7 @@ $modal = "";
                                 .multiPic' . $image->id . '{ 
                                     width:350px; 
                                     height:240px;  
-                                    background: url(' . getenv('OPENSHIFT_REPO_DIR') .'uploads/blue_bird.jpg) no-repeat;
+                                    background: url(uploads/'. $image->image . ') no-repeat;
                                     background-size: 350px 240px;
                                 } 
                                 .multiPic' . $image->id . ' .text' . $image->id . '{ 
