5a170c11de16ee516d710af230a571bf3ec18fc4
Eric Gonzalez <Twinpaireric@gmail.com>
2016-08-01 22:56:42 -0700

    Update

--- download.php
+++ download.php
@@ -5,7 +5,7 @@ header('Content-type: multipart/form-data');
 //open/save dialog box
 header('Content-Disposition: attachment; filename="' . $_POST['name'] . '"');
 //read from server and write to buffer
-readfile("http://imag-uana.rhcloud.com/uploads/uploads/" . $_POST['name']);
+readfile("uploads/uploads/" . $_POST['name']);
 
 ?>
 
