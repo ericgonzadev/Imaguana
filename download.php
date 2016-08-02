63c913748a42f1be757c225bbf162d4bba41dddc
Eric Gonzalez <Twinpaireric@gmail.com>
2016-08-01 22:51:34 -0700

    Update

--- download.php
+++ download.php
@@ -3,9 +3,9 @@
 //content type
 header('Content-type: multipart/form-data');
 //open/save dialog box
-header('Content-Disposition: attachment; filename="' getenv('OPENSHIFT_DATA_DIR'). "/uploads/" . $_POST['name'] . '"');
+header('Content-Disposition: attachment; filename="' . $_POST['name'] . '"');
 //read from server and write to buffer
-readfile(getenv('OPENSHIFT_DATA_DIR'). "/uploads/" . $_POST['name']);
+readfile("http://imag-uana.rhcloud.com/uploads/uploads/" . $_POST['name']);
 
 ?>
 
