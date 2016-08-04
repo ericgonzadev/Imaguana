6956a0927b27a50cd99d50538bf4189b2e6203bb
Eric Gonzalez <Twinpaireric@gmail.com>
2016-08-03 22:14:13 -0700

    Debugging

--- preview.php
+++ preview.php
@@ -99,6 +99,63 @@ $validations = "";
                     $image_path = resize($ext, $width, $height,  $image_path, $filename, "3");
                 }
             }
+
+            function resize($ext, $width, $height, $image_path, $filename, $size){
+        //Create medium size picture
+        switch ($ext){
+            case "png":
+                $src= imagecreatefrompng($image_path);
+                break;
+            case "gif":
+                $src= imagecreatefromgif($image_path);
+                break;
+            default:
+                $src= imagecreatefromjpeg($image_path);
+        }
+        if ($size == "1.5"){
+            $newWidth = $width/1.5;
+            $newHeight = $height/1.5;
+        }
+        else if ($size == "2"){
+            $newWidth = $width/2;
+            $newHeight = $height/2;
+        }
+        else if ($size == "2.5"){
+            $newWidth = $width/2.5;
+            $newHeight = $height/2.5;
+        }
+        else if ($size == "3"){
+            $newWidth = $width/3;
+            $newHeight = $height/3;
+        }
+        else if ($size == "3.5"){
+            $newWidth = $width/3.5;
+            $newHeight = $height/3.5;
+        }
+        else if ($size == "4"){
+            $newWidth = $width/4;
+            $newHeight = $height/4;
+        }
+        else if ($size == "4.5"){
+            $newWidth = $width/4.5;
+            $newHeight = $height/4.5;
+        }
+        $tmp = imagecreatetruecolor($newWidth, $newHeight);
+        imagecopyresampled($tmp, $src, 0,0,0,0, $newWidth, $newHeight, $width, $height);
+        switch ($ext){
+            case "png":
+                imagepng($tmp, $image_path , 9);
+                break;
+            case "gif":
+                imagegif($tmp, $image_path , 100);
+                break;
+            default:
+                imagejpeg($tmp, $image_path , 100);
+        }
+        imagedestroy($src);
+        imagedestroy($tmp);
+        return $image_path;
+    }
         ?>
             
             <div class="col-sm-11" style="text-align: center; margin-top: -20px; margin-left: 24px;" >
