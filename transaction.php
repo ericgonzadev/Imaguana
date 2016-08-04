453a4047450106aac6086c77726cc5907bf96aac
Eric Gonzalez <Twinpaireric@gmail.com>
2016-08-03 20:17:35 -0700

    debugging

--- transaction.php
+++ transaction.php
@@ -47,7 +47,78 @@ else{
     //check if user already bought the license
     $result = $conn->query("SELECT * FROM transactions WHERE user_id = " . $user->data()->id . " AND image_id = " . $image->data()->id . " AND license = 'unlimited'");
     print_r($result->num_rows);
-    
+    if ($result->num_rows > 0) {
+        header("location: ./purchases.php#" . $image->data()->id);
+    }
+
+    $result = $conn->query("SELECT * FROM transactions WHERE user_id = " . $user->data()->id . " AND image_id = " . $image->data()->id . " AND license = '" . $_POST['license'] . "'");
+    if ($result->num_rows > 0) {
+        header("location: ./purchases.php#" . $image->data()->id);
+    }
+    else{
+        //change license to unlimited if user buys both licenses seperately
+        $result = $conn->query("SELECT * FROM transactions WHERE user_id = " . $user->data()->id . " AND image_id = " . $image->data()->id . " AND license = 'print'");
+        if(($_POST['license'] == "web" || $_POST['license'] == "unlimited") && $result->num_rows > 0){
+            $license = "unlimited";
+            $conn->query("DELETE FROM transactions WHERE user_id = " . $user->data()->id . " AND image_id = " . $image->data()->id . " AND license = 'print'");
+            // prepare and bind
+            $stmt = $conn->prepare("INSERT INTO transactions (user_id, image_id, license, price, type) VALUES (?, ?, ?, ?, ?)");
+            $stmt->bind_param("iisis", $user_id, $image_id, $license, $price, $type);
+
+            // set parameters and execute
+            $user_id = $user->data()->id;
+            $image_id = $image->data()->id;
+            $license;
+            $price = $image->data()->$license;
+            $type = "image";
+
+            $stmt->execute();
+            $stmt->close();
+            $conn->close();
+
+            header("location: ./purchases.php#" . $image->data()->id);
+        }
+
+        $result = $conn->query("SELECT * FROM transactions WHERE user_id = " . $user->data()->id . " AND image_id = " . $image->data()->id . " AND license = 'web'");
+        if(($_POST['license'] == "print" || $_POST['license'] == "unlimited") && $result->num_rows > 0){
+            $license = "unlimited";
+            $conn->query("DELETE FROM transactions WHERE user_id = " . $user->data()->id . " AND image_id = " . $image->data()->id . " AND license = 'web'");
+            // prepare and bind
+        $stmt = $conn->prepare("INSERT INTO transactions (user_id, image_id, license, price, type) VALUES (?, ?, ?, ?, ?)");
+        $stmt->bind_param("iisis", $user_id, $image_id, $license, $price, $type);
+
+        // set parameters and execute
+        $user_id = $user->data()->id;
+        $image_id = $image->data()->id;
+        $license;
+        $price = $image->data()->$license;
+        $type = "image";
+
+        $stmt->execute();
+        $stmt->close();
+        $conn->close();
+
+        header("location: ./purchases.php#" . $image->data()->id);
+        }
+        else{
+            // prepare and bind
+            $stmt = $conn->prepare("INSERT INTO transactions (user_id, image_id, license, price, type) VALUES (?, ?, ?, ?, ?)");
+            $stmt->bind_param("iisis", $user_id, $image_id, $license, $price, $type);
+
+            // set parameters and execute
+            $user_id = $user->data()->id;
+            $image_id = $image->data()->id;
+            $license;
+            $price = $image->data()->$license;
+            $type = "image";
+
+            $stmt->execute();
+            $stmt->close();
+            $conn->close();
+
+            header("location: ./purchases.php#" . $image->data()->id);
+        }
+    }
 }
 ?>
         
