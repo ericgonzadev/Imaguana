9a7c5ae0e260891737a8404a982770f65b317c33
Eric <twinpaireric@gmail.com>
2017-10-02 19:11:40 -0700

    Remove double uploads folder and single it to one

--- download.php
+++ download.php
@@ -5,7 +5,7 @@ header('Content-type: multipart/form-data');
 //open/save dialog box
 header('Content-Disposition: attachment; filename="' . $_POST['image'] . '"');
 //read from server and write to buffer
-readfile("uploads/uploads/" . $_POST['image']);
+readfile("uploads/" . $_POST['image']);
 
 ?>
 
--- imageupload.php
+++ imageupload.php
@@ -110,7 +110,7 @@ if ($user->data()->group != 1) {
                     'web' => $web,
                     'print' => $print,
                     'unlimited' => $unlimited,
-                    'image' => "uploads/uploads/" . $filename,
+                    'image' => "uploads/" . $filename,
 		            'category' => $category,
                     'tags' => $tags ));
             header("location: /image/show/". $lastid);
